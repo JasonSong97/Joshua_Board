e8d7263674f23ee9664519bbd165ace11085caa6
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-13 15:54:44 +0900

    setting : 시큐리티 세팅

---
+++ src/main/java/coffee/pastry/joshuablog/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package coffee.pastry.joshuablog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+
+     @Bean
+     BCryptPasswordEncoder passwordEncoder() {
+          return new BCryptPasswordEncoder();
+     }
+
+     @Bean
+     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+          http.csrf().disable();
+          http.formLogin()
+                    .loginPage("/loginForm")
+                    .loginProcessingUrl("/login")
+                    .successHandler((request, response, authentication) -> {
+                         log.debug("디버그 : 로그인 성공");
+
+                    })
+                    .failureHandler((request, response, exception) -> {
+                         log.debug("디버그 : 로그인 실패 : " + exception.getMessage());
+
+                    });
+          http.authorizeRequests(
+                    authorize -> authorize.antMatchers("/auth/**").authenticated()
+                              .anyRequest().permitAll());
+          return http.build();
+     }
+}
